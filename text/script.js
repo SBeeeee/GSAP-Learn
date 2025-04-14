858fc74bbe60fed8179a23649031b54cdf672853
SBeeeee <shatadru017@gmail.com>
2025-04-14 17:48:19 +0530

    added text part 2

--- text/script.js
+++ text/script.js
@@ -1,19 +1,32 @@
 function breaktext() {
     var h1 = document.querySelector("h1");
     var h1text = h1.textContent;
-    console.log(h1text)
+    
     var splittedtext = h1text.split("");
+    var halfValue=splittedtext.length/2
+
     var clutter = "";
 
-    splittedtext.forEach(function (e) {
-        clutter = clutter+`<span>${e}</span>`
-
+    splittedtext.forEach(function (e,i) {
+       if(i<halfValue){
+         clutter = clutter+`<span class="a">${e}</span>`
+       }
+       else{
+       clutter= clutter+`<span class="b">${e}</span>`
+       }
     })
 
     h1.innerHTML = clutter
 }
  breaktext()
-gsap.from("h1 span",{
+gsap.from("h1 .a",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.15,
+})
+
+gsap.from("h1 .b",{
     y:50,
     opacity:0,
     duration:1,
