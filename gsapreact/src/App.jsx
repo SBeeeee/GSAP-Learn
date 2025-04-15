4e42e4bdbe4e215af24a9fa148e5a136ebc80b6d
SBeeeee <shatadru017@gmail.com>
2025-04-15 11:03:36 +0530

    proper workflow given

--- gsapreact/src/App.jsx
+++ gsapreact/src/App.jsx
@@ -1,27 +1,22 @@
-import React, { useRef } from 'react'
+import React, { useRef,useState } from 'react'
 import {useGSAP} from '@gsap/react'
 import gsap from 'gsap'
 
 const App = () => {
   
-  const boxRef=useRef();
+  const [circle,setCircle]=useState(60)
+  const random=gsap.utils.random(-500,500,10)
+
   useGSAP(()=>{
-    gsap.from(boxRef.current,{
-      rotate:720,
-      duration:2,
+    gsap.to(".circle",{
+      x:circle,
+      duration:0.5
     })
-  })
+  },[circle])  //{scope:".container or boxRef" ,dependencies:[xValue,yValue]  //syntax to use both scope and dependencies scope defines the portion where the animation shld search for given classes or id to animate
   return (
     <main>
-     <div className="container">
-   
-     <div className="circle"></div>
-     <div ref={boxRef} className="box"></div>
-     </div>
-     <div className="kuch">
-      <div className="circle"></div>
-      <div className="box"></div>
-     </div>
+    <button onClick={()=>{setCircle(random)}} >Animate</button>
+    <div className="circle"></div>
     </main>
   )
 }
