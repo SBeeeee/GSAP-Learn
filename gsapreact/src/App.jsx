3406a286737e79b02e3b0c593755510b00310cb6
SBeeeee <shatadru017@gmail.com>
2025-04-15 08:12:00 +0530

    gsap1

--- gsapreact/src/App.jsx
+++ gsapreact/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+  
   useGSAP(()=>{
-    
+    gsap.to(".box",{
+      x:1000,
+      duration:2,
+      delay:1
+    })
   })
 
   return (
